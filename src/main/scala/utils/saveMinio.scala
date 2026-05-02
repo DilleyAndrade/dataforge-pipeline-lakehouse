d01e1e5bdeb9ba7355976c405b883173cddfff3c
Dilley Andrade <dilleyandrade@gmail.com>
2026-05-01 23:41:23 -0300

    feature: added create log function

---
+++ src/main/scala/utils/saveMinio.scala
@@ -0,0 +1,12 @@
+package utils
+
+import org.apache.spark.sql.DataFrame
+
+object saveMinio {
+  def save_minio(dataframe: DataFrame, save_mode: String, format: String, path: String): Unit = {
+    dataframe.write
+      .mode(save_mode)
+      .format(format)
+      .save(path)
+  }
+}
