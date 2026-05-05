e7846ceb90ee26dc23088cdca9bfd73985670ed2
Dilley Andrade <dilleyandrade@gmail.com>
2026-05-04 21:35:15 -0300

    fix: minio env variables readed inside

--- src/main/scala/connectors/minioConn.scala
+++ src/main/scala/connectors/minioConn.scala
@@ -2,19 +2,21 @@ package connectors
 
 import io.minio.MinioClient
 import org.apache.spark.sql.SparkSession
-
+import io.github.cdimascio.dotenv.Dotenv
 
 object minioConn {
-  def minio_connection(spark:SparkSession, minio_endpoint:String, minio_access_key:String, minio_secret_key:String): Unit = {
+  def minio_connection(spark:SparkSession, minio_endpoint:String): Unit = {
+
+    val dotenv = Dotenv.load()
+    val minio_access_key = dotenv.get("MINIO_ACCESS_KEY")
+    val minio_secret_key = dotenv.get("MINIO_SECRET_KEY")
 
     val endpoint = minio_endpoint
-    val accessKey = minio_access_key
-    val secretKey = minio_secret_key
 
     val hadoopConf = spark.sparkContext.hadoopConfiguration
 
-    hadoopConf.set("fs.s3a.access.key", accessKey)
-    hadoopConf.set("fs.s3a.secret.key", secretKey)
+    hadoopConf.set("fs.s3a.access.key", minio_access_key)
+    hadoopConf.set("fs.s3a.secret.key", minio_secret_key)
     hadoopConf.set("fs.s3a.endpoint", endpoint)
     hadoopConf.set("fs.s3a.path.style.access", "true")
     hadoopConf.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
@@ -24,14 +26,14 @@ object minioConn {
     try {
       val client = MinioClient.builder()
         .endpoint(endpoint)
-        .credentials(accessKey, secretKey)
+        .credentials(minio_access_key, minio_secret_key)
         .build()
 
       // Connection test
       val buckets = client.listBuckets()
 
       println(line_spaces)
-      println("MinIO connection successfully!")
+      println("MINIO connection successfully!")
       println(s"Buckets found: ${buckets.size()}")
 
     } catch {
