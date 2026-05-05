78eb7fa033b6da12c560000308b3ab97b1a867a5
Dilley Andrade <dilleyandrade@gmail.com>
2026-05-04 21:36:44 -0300

    feature: added constants with main paths

---
+++ src/main/scala/constantsPath/constantsPath.scala
@@ -0,0 +1,8 @@
+package constantsPath
+
+
+object constantsPath {
+
+  val PATH_SAVE_JSON_FILE_TRANSACTIONS = s"s3a://bronze/transactions/"
+  val PATH_SAVE_LOG_FILE_TRANSACTIONS = s"s3a://logs/transactions/"
+}
